f0d2b25fcbad7174f93b78053084c4fcc0b6095f
Ruslan Valeev <ruvaleev@gmail.com>
2023-11-15 16:31:25 -0300

    Fix sqrtPriceToReadablePrices function

--- __tests__/services/sqrtPriceToReadablePrices.test.js
+++ __tests__/services/sqrtPriceToReadablePrices.test.js
@@ -31,4 +31,19 @@ describe('sqrtPriceToReadablePrices', () => {
       })
     });
   })
+
+  describe("with decimals0 = '8' and decimals1 = '18' and liquidity = '33937225031909479086361535233395569'", () => {
+    const sqrtPrice = '33937225031909479086361535233395569';
+    const decimals0 = '8';
+    const decimals1 = '18';
+
+    it('returns properly calculated prices', () => {
+      expect(
+        sqrtPriceToReadablePrices(sqrtPrice, decimals0, decimals1)
+      ).toEqual({
+        price0: BigNumber('18.34820099176733050163617104052222906961907690524641'),
+        price1: BigNumber('0.05450125603314956208')
+      })
+    });
+  });
 });
--- src/services/sqrtPriceToReadablePrices.js
+++ src/services/sqrtPriceToReadablePrices.js
@@ -5,7 +5,7 @@ const sqrtPriceToReadablePrices = (sqrtPriceString, decimals0, decimals1) => {
   const sqrtPrice = BigNumber(sqrtPriceString);
   const sqrtPriceRatio = sqrtPrice.dividedBy(Q96);
 
-  const price0 = sqrtPriceRatio.pow(2).multipliedBy(BigNumber(10).pow(Math.abs(decimals0 - decimals1)));
+  const price0 = sqrtPriceRatio.pow(2).multipliedBy(BigNumber(10).pow(decimals0 - decimals1));
   const price1 = BigNumber(1).dividedBy(price0);
 
   return { price0, price1 }
