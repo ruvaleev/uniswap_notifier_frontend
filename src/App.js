880c1a0caf3d2c616404eb42120211b273d11003
Ruslan Valeev <ruvaleev@gmail.com>
2023-12-26 14:28:45 -0600

    Add Rollbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Provider, ErrorBoundary } from '@rollbar/react'; // Provider imports 'rollbar'
 
 import { WalletProvider } from './contexts/WalletContext';
 import Main from './pages/Main';
 
+const rollbarConfig = {
+  accessToken: process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
+  environment: process.env.NODE_ENV,
+  enabled: process.env.ROLLBAR_ENABLED === 'true'
+};
+
 function App() {
   return (
-    <WalletProvider>
-      <Main/>
-    </WalletProvider>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <WalletProvider>
+          <Main/>
+        </WalletProvider>
+      </ErrorBoundary>
+    </Provider>
   );
 }
 
