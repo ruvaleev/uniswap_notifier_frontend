0e6eeab5d2a813955a74d1c7c017d2521c528835
Ruslan Valeev <ruvaleev@gmail.com>
2023-12-30 13:09:42 -0600

    Add tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles.css';
+import './styles/styles.css';
+import './styles/tailwindOutput.css';
 import App from './App';
 
 const root = document.getElementById('app');
